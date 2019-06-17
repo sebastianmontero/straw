89ed2030b91b6e3b28340f1d1835bf06e1ea9e3e
Sebastian Montero <smontero@mightywizards.com>
2019-06-17 13:24:23 -0500

    Replaced the redis npm package for the promise-redis package

--- test/runner_test.js
+++ test/runner_test.js
@@ -1,14 +1,15 @@
 "use strict";
 
 var Runner = require('../lib/runner.js');
-var redis = require('redis');
+var redis = require('promise-redis')();
 
 var opts = {
-  redis: {
-    host: '127.0.0.1',
-    port: 6379,
-    prefix: 'straw-test'
-  }};
+    redis: {
+        host: '127.0.0.1',
+        port: 6379,
+        prefix: 'straw-test'
+    }
+};
 
 
 // init
@@ -20,76 +21,76 @@ var opts = {
 // initialize, start, reboot
 
 exports['runner'] = {
-  'init-quit': function(test) {
+    'init-quit': function (test) {
 
-    var runner;
+        var runner;
 
-    //test.expect(3);
-    var node = {
-      id: 'test',
-      node: __dirname + '/../examples/nodes/thru.js',
-      input: ['test-to-node'],
-      output: ['test-from-node']
-    };
+        //test.expect(3);
+        var node = {
+            id: 'test',
+            node: __dirname + '/../examples/nodes/thru.js',
+            input: ['test-to-node'],
+            output: ['test-from-node']
+        };
 
-    runner = new Runner (
-      node,
-      opts,
-      function(err, pid) {
-        //console.log('PID', pid);
-        runner.quit(function(){
-          test.done();
-        });
-      });
-  },
-  'init-start-quit': function(test) {
+        runner = new Runner(
+            node,
+            opts,
+            function (err, pid) {
+                //console.log('PID', pid);
+                runner.quit(function () {
+                    test.done();
+                });
+            });
+    },
+    'init-start-quit': function (test) {
 
-    var runner;
+        var runner;
 
-    // start a runner, wait for it to emit a message via Redis then
-    // quit.
+        // start a runner, wait for it to emit a message via Redis then
+        // quit.
 
-    //test.expect(3);
+        //test.expect(3);
 
-    var node = {
-      id: 'ping',
-      node: __dirname + '/../examples/nodes/ping.js',
-      //input: ['test-to-node'],
-      output: ['test-from-node']
-    };
+        var node = {
+            id: 'ping',
+            node: __dirname + '/../examples/nodes/ping.js',
+            //input: ['test-to-node'],
+            output: ['test-from-node']
+        };
 
-    var pipe = opts.redis.prefix + ':' + node.output[0];
+        var pipe = opts.redis.prefix + ':' + node.output[0];
 
-    //var redisIn = redis.createClient();
-    var redisFromNode = redis.createClient();
+        //var redisIn = redis.createClient();
+        var redisFromNode = redis.createClient();
 
-    var finished = function(){
-      runner.quit(function(){
-        redisFromNode.quit();
-        test.done();
-      });
-    };
+        var finished = function () {
+            runner.quit(function () {
+                redisFromNode.quit();
+                test.done();
+            });
+        };
 
-    var brpop = function(){
-      var lkeys = [pipe, 5];
-      redisFromNode.brpop(
-        lkeys,
-        function(err, reply){
-          test.equal(reply[0], pipe);
-          var msg = JSON.parse(reply[1]);
-          test.ok(msg.hasOwnProperty('ping'));
-          finished();
-        });
-    };
+        var brpop = function () {
+            var lkeys = [pipe, 5];
+            redisFromNode.brpop(
+                lkeys,
+                function (err, reply) {
+                    test.equal(reply[0], pipe);
+                    var msg = JSON.parse(reply[1]);
+                    test.ok(msg.hasOwnProperty('ping'));
+                    finished();
+                });
+        };
 
-    runner = new Runner (
-      node,
-      opts,
-      function(err, pid) {
-        runner.start(function(){
-          brpop();
-        });
-      });
-  }
+        runner = new Runner(
+            node,
+            opts,
+            function (err, pid) {
+                runner.start(function () {
+                    brpop();
+                });
+            });
+    }
 
 };
